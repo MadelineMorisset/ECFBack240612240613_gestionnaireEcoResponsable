7a59235c7f3c2c2f5b0f82be35f8c51fb6c56e71
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-06-13 11:52:50 +0200

    Index file

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/resetCss.css">
+    <link rel="stylesheet" href="./assets/css/variable.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!--  FONT - Lora : Title (https://fonts.google.com/specimen/Lora?stroke=Serif) -->
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+        rel="stylesheet">
+    <!-- FONT - Josefin Sans : Text (https://fonts.google.com/specimen/Josefin+Sans?stroke=Sans+Serif&subset=latin&stylecount=12&noto.script=Latn) -->
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+    <title>Gestionnaire Éco-Responsable</title>
+</head>
+
+<body>
+    <?php include("./view/header.php") ?>
+
+</body>
+
+</html>
